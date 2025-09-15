ad745548fa7a18aa8771c5e34e1eebc7eeb26964
riyaan-bai <riyaanb2306@gmail.com>
2025-09-16 06:30:00 +0700

    Tugas 3

--- main/models.py
+++ main/models.py
@@ -3,30 +3,22 @@ from django.db import models
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('fitness equipment', 'Fitness Equipment'),
-        ('socks', 'Socks'),
-        ('ball', 'Ball'),
-        ('shoes', 'Shoes'),
-        ('accessories', 'Accessories'),
-        ('clothes', 'Clothes'),
+        ('sportswear', 'Sportswear'),
+        ('sports shoes & sandals', 'Sports Shoes & Sandals'),
+        ('sports sccessories', 'Sports Accessories'),
+        ('sports equipment', 'Sports Equipment'),
+        ('health & fitness support', 'Health & Fitness Support'),
+        ('protective gear', 'Protective Gear'),
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
     description = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, default='update')
     thumbnail = models.URLField(blank=True, null=True)
     price = models.IntegerField(default=0)
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
         return self.title
-    
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
